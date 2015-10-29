202decc523a6adb345b018047c4560a98f0514f7
Duane Jeffers <duane@duanejeffers.com>
2015-10-29 13:34:42 -0500

    Updated to use Query models.

--- src/Client.php
+++ src/Client.php
@@ -4,16 +4,18 @@
 namespace ElevateAPI;
 
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
+use ElevateAPI\Query\QueryAbstract;
 
 class Client
 {
     protected $_client;
     protected $_parameters;
     protected $_auth_key;
-    protected $_security = FALSE;
+    protected $_security = 'key';
     
     // Constructor for the client class.
-    public function __construct($base_url, $auth_key, $security = FALSE) {
+    public function __construct($base_url, $auth_key, $security = 'key') {
         $this->_client = new GuzzleClient(['base_uri' => $base_url]);
         
         $this->_auth_key = $auth_key;
@@ -21,12 +23,23 @@ class Client
         $this->_security = $security;
     }
     
-    public function get($endpoint, array $parameters) {
+    public function get(QueryAbstract $query, array $parameters) {
         // Merge Parameters with the auth.
-        if(!$this->_security) {
-            $parameters['auth'] = $this->_auth_key;
+        $parameters['auth'] = $query->authSecurityString($this->_security, $this->_auth_key);
+        
+        // grab the endpoint from the query class.
+        $endpoint = 'solar.qll_web.' . $query->query;
+        
+        try {
+            $response = $this->_client->request('GET', $endpoint, ['query' => $parameters]);
+        } catch(GuzzleException $e) {
+            throw $e;
         }
         
-        return $this->_client->request('GET', $endpoint, $parameters);
+        if($response->getStatusCode() == '200') {
+            $xml = $response->getBody()->getContents();
+            
+            return new SimpleXMLElement($xml);
+        }
     }
 }
