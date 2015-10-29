e2ef15a6da5bbd8511718dbd1e6121765f1b3c7b
Duane Jeffers <duane@duanejeffers.com>
2015-10-29 08:20:11 -0500

    Initial Code Commit for the API package.

---
+++ src/Client.php
@@ -0,0 +1,32 @@
+<?php
+
+    // Elevate API Client
+namespace ElevateAPI;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+class Client
+{
+    protected $_client;
+    protected $_parameters;
+    protected $_auth_key;
+    protected $_security = FALSE;
+    
+    // Constructor for the client class.
+    public function __construct($base_url, $auth_key, $security = FALSE) {
+        $this->_client = new GuzzleClient(['base_uri' => $base_url]);
+        
+        $this->_auth_key = $auth_key;
+        
+        $this->_security = $security;
+    }
+    
+    public function get($endpoint, array $parameters) {
+        // Merge Parameters with the auth.
+        if(!$security) {
+            $parameters['auth'] = $this->_auth_key;
+        }
+        
+        $result = $this->_client->request('GET', $endpoint, $parameters);
+    }
+}
